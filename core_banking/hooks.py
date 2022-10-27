e591f7e48d8c4dce7beaecee965778f5cd2aeead
Baba Zaha <dsmwaura@gmail.com>
2022-10-28 00:42:11 +0300

    Portal customization

--- core_banking/hooks.py
+++ core_banking/hooks.py
@@ -1,4 +1,5 @@
 from . import __version__ as app_version
+from frappe import _
 
 app_name = "core_banking"
 app_title = "Core Banking"
@@ -23,6 +24,16 @@ app_include_js = [
     "/assets/core_banking/js/custom_scripts/payroll_entry.js",
 ]
 
+
+website_route_rules=[
+    {"from_route": "/rfq", "to_route": "Request for Quotation"},
+	{"from_route": "/rfq/<path:name>", "to_route": "rfq",
+		"defaults": {
+			"doctype": "Request for Quotation",
+			"parents": [{"label": _("Request for Quotation"), "route": "rfq"}]
+		}
+	},
+]
 # include js, css files in header of web template
 # web_include_css = "/assets/core_banking/css/core_banking.css"
 # web_include_js = "/assets/core_banking/js/core_banking.js"
