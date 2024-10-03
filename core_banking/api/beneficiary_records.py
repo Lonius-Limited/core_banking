c6123f54b29d5605d4db33bd433667bb0a2b9cef
Baba <dsmwaura@gmail.com>
2024-10-04 00:45:10 +0300

    Added Upsert function for searchof  eneficiary

--- core_banking/api/beneficiary_records.py
+++ core_banking/api/beneficiary_records.py
@@ -1,53 +1,94 @@
 import frappe
 from frappe.utils import now_datetime, today, flt
-import traceback
+import traceback, json
+from core_banking.api.customer import create_beneficiary
+
+
+def _default_invoice_item():
+    _item = frappe.db.get_single_value("Core Banking Settings", "default_invoice_item")
+    if not _item:
+        args = dict(
+            doctype="Item",
+            item_code="SHIF Contribution",
+            item_name="SHIF Contribution",
+            item_group="Services",
+            maintain_stock=0,
+        )
+        frappe.get_doc(args).insert()
+        _settings = frappe.get_doc("Core Banking Settings")
+        _settings.set("default_invoice_item", "SHIF Contribution")
+        _settings.save()
+        frappe.db.commit()
+        _item = "SHIF Contribution"
+    return _item
+
 
 @frappe.whitelist()
 def post_payment_notification(**kwargs):
     try:
-        cr_number = kwargs.get('cr_number')
-        id_number = kwargs.get('id_number')
-        amount = kwargs.get('amount')
+        if isinstance(kwargs, str):
+            kwargs = json.loads(kwargs)
+        kwargs.pop("cmd", None)
+        cr_number = kwargs.pop("id", None)
+        other_identifiers = kwargs.pop("other_identifications")
+        _household_id = list(
+            filter(
+                lambda x: x.get("identification_type") == "Household Number",
+                other_identifiers,
+            )
+        )[0]["identification_number"]
+        identification_type = kwargs.get("identification_type")
+        identification_number = kwargs.get("identification_number")
+        amount = kwargs.get("amount")
 
         missing_fields = []
-        if not (cr_number or id_number):
+        if not (cr_number or identification_number):
             missing_fields.append("CR Number or ID Number")
         if not amount:
             missing_fields.append("Amount")
 
         if missing_fields:
             return {
-                "status": False, 
-                "message": f"The following field(s) are required: {', '.join(missing_fields)}"
+                "status": False,
+                "message": f"Either of the following field(s) are required: {', '.join(missing_fields)}",
             }
 
         try:
             amount = flt(amount)
         except ValueError:
             return {"status": False, "message": "Invalid amount provided"}
+        #Check if customer exists and create if not
+        customer_args = dict(name=cr_number)
+        if identification_number:
+            customer_args = dict(
+                custom_identification_type=identification_type,
+                custom_identification_number=identification_number,
+            )
+        _customer = frappe.db.get_value("Customer", customer_args)
+        if not _customer:
+            create_beneficiary_payload = {
+                **kwargs,
+                "id": cr_number,
+                "household_number": _household_id,
+            }
+            _customer = create_beneficiary(**create_beneficiary_payload)
+            # return dict(status="Pending", reason="We have scheduled")
 
-        # Get Customer
-        if cr_number:
-            if not frappe.db.exists("Customer", cr_number):
-                return {"status": False, "message": f"Customer with CR Number {cr_number} does not exist"}
-            customer_name = cr_number
-        else:
-            customer = frappe.get_list("Customer", filters={"cr_number": id_number}, fields=["name"])
-            if not customer:
-                return {"status": False, "message": f"Customer with CR Number {id_number} does not exist"}
-            customer_name = customer[0].name
-
+        customer_name = _customer
         # Sales Invoice
         invoice = frappe.new_doc("Sales Invoice")
         invoice.customer = customer_name
         invoice.date = today()
         invoice.posting_time = now_datetime().time()
-        invoice.append("items", {
-            "item_code": "SHA PAYMENT",
-            "qty": 1,
-            "rate": amount,
-            "amount": amount
-        })
+        invoice.append(
+            "items",
+            {
+                "item_code": _default_invoice_item(),
+                "qty": 1,
+                "rate": amount,
+                "amount": amount,
+            },
+        )
         invoice.insert()
         invoice.submit()
 
@@ -72,17 +113,24 @@ def post_payment_notification(**kwargs):
         payment_entry.target_exchange_rate = 1
 
         # accounts
-        payment_entry.paid_to = frappe.get_value("Account", {"account_type": "Cash", "company": invoice.company, "is_group": 0}, "name")
+        payment_entry.paid_to = frappe.get_value(
+            "Account",
+            {"account_type": "Cash", "company": invoice.company, "is_group": 0},
+            "name",
+        )
         payment_entry.paid_from = invoice.debit_to
 
         # payment link to invoice
-        payment_entry.append("references", {
-            "reference_doctype": "Sales Invoice",
-            "reference_name": invoice.name,
-            "total_amount": amount,
-            "outstanding_amount": amount,
-            "allocated_amount": amount
-        })
+        payment_entry.append(
+            "references",
+            {
+                "reference_doctype": "Sales Invoice",
+                "reference_name": invoice.name,
+                "total_amount": amount,
+                "outstanding_amount": amount,
+                "allocated_amount": amount,
+            },
+        )
 
         payment_entry.insert()
         payment_entry.submit()
@@ -92,10 +140,10 @@ def post_payment_notification(**kwargs):
         return {
             "status": True,
             "message": "Generated successfully",
-            "entry_ref": payment_entry.name
+            "entry_ref": payment_entry.name,
         }
 
     except Exception as e:
         frappe.db.rollback()
         frappe.log_error(traceback.format_exc(), "Entry Generation Error")
-        return {"status": False, "message": str(e), "traceback": traceback.format_exc()}
+        return {"status": False, "message": str(e)}
--- core_banking/api/customer.py
+++ core_banking/api/customer.py
@@ -2,23 +2,29 @@ import frappe
 from frappe.utils import now_datetime
 import traceback
 
+
 @frappe.whitelist()
 def create_customer(**kwargs):
     try:
-        required_fields = ['cr_number', 'household_number', 'customer_name', 'identification_number']
+        required_fields = [
+            "cr_number",
+            "household_number",
+            "customer_name",
+            "identification_number",
+        ]
         missing_fields = [field for field in required_fields if not kwargs.get(field)]
 
         if missing_fields:
             return {
                 "status": False,
                 "message": f"Missing required fields: {', '.join(missing_fields)}",
-                "missing_fields": missing_fields
+                "missing_fields": missing_fields,
             }
 
-        cr_number = kwargs['cr_number']
-        household_number = kwargs['household_number']
-        customer_name = kwargs['customer_name']
-        identification_number = kwargs['identification_number']
+        cr_number = kwargs["cr_number"]
+        household_number = kwargs["household_number"]
+        customer_name = kwargs["customer_name"]
+        identification_number = kwargs["identification_number"]
 
         # Check if customer with cr_number already exists
         if frappe.db.exists("Customer", {"customer_name": cr_number}):
@@ -26,8 +32,7 @@ def create_customer(**kwargs):
                 "status": False,
                 "message": f"Customer with CR Number {cr_number} already exists.",
             }
-        
-        
+
         # Check if customer group (household) exists, create if not
         if not frappe.db.exists("Customer Group", household_number):
             customer_group = frappe.new_doc("Customer Group")
@@ -36,32 +41,72 @@ def create_customer(**kwargs):
             customer_group.insert()
 
         # Create customer
-        customer = frappe.get_doc({
-            "doctype": "Customer",
-            "customer_name": identification_number,
-            "customer_full_names":customer_name,
-            # "cr_number": cr_number,
-            "customer_type": "Individual",
-            "customer_group": household_number,
-            "territory": "Kenya",
-            "identification_type": "National ID",
-            "identification_number": identification_number
-        })
-       
+        customer = frappe.get_doc(
+            {
+                "doctype": "Customer",
+                "customer_name": identification_number,
+                "customer_full_names": customer_name,
+                # "cr_number": cr_number,
+                "customer_type": "Individual",
+                "customer_group": household_number,
+                "territory": "Kenya",
+                "identification_type": "National ID",
+                "identification_number": identification_number,
+            }
+        )
+
         customer.insert()
 
         frappe.db.commit()
         return {
             "status": True,
             "message": "Customer created successfully",
-            "customer": customer.name
+            "customer": customer.name,
         }
 
     except Exception as e:
         frappe.db.rollback()
         frappe.log_error(traceback.format_exc(), "Customer Creation Error")
-        return {
-            "status": False,
-            "message": str(e),
-            "traceback": traceback.format_exc()
-        }
+        return {"status": False, "message": str(e), "traceback": traceback.format_exc()}
+
+
+def create_beneficiary(**kwargs):
+    mandatory_fields = [
+        "id",
+        "identification_type",
+        "identification_number",
+        "full_name",
+        "household_number",
+        "employment_type",
+    ]
+    _missing_variables = []
+    for k in mandatory_fields:
+        if k not in list(kwargs.keys()):
+            _missing_variables.append(k)
+    if _missing_variables: frappe.throw("Missing values {}".format(_missing_variables))
+    
+    # Check if customer group (household) exists, create if not
+    if not frappe.db.exists("Customer Group",  kwargs.get("household_number")):
+        customer_group = frappe.new_doc("Customer Group")
+        customer_group.customer_group_name =  kwargs.get("household_number")
+        customer_group.parent_customer_group = "All Customer Groups"
+        customer_group.insert()
+    # Begin transaction 
+    customer_args = {
+                "doctype": "Customer",
+                "customer_name": kwargs.get("id"),
+                "custom_customer_full_name": kwargs.get("full_name"),
+                # "cr_number": cr_number,
+                "customer_type": "Individual",
+                "customer_group": kwargs.get("household_number"),
+                "territory": "Kenya",
+                "custom_identification_type": kwargs.get("identification_type"),
+                "custom_identification_number": kwargs.get("identification_number"),
+                "custom_employment_status": kwargs.get("employment_type")
+            }
+    customer = frappe.get_doc(customer_args)
+    customer.db_insert()
+    frappe.db.commit()
+    return customer.get("name")
+    
+        
